c2f2271704fda10e4297a8a399b3ed8f64c61ca6
Musa Mahmudov <mr.musa.mahmudov@mail.ru>
2026-04-14 00:42:49 +0400

    my changes added

---
+++ src/CORE/Entities/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PsychologyApi.Core.Entities.Common;
+
+namespace PsychologyApi.Core.Entities
+{
+    public class Question : BaseEntity
+    {
+        public string Text { get; set; } 
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+        public ICollection<Answer> Answers { get; set; }
+    }
+}
---
+++ src/CORE/Entities/Repositories/IAnswerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PsychologyApi.Core.Entities;
+
+namespace PsychologyApi.Core.Repositories
+{
+    public interface IAnswerRepository : IGenericRepository<Answer>
+    {
+    }
+}
